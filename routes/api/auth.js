2c2f0a49d57c255c94d884499694129ab5d92324
Pedja1310 <pedja1310@gmail.com>
2019-07-08 23:56:43 +0200

    Create and update new profile added.

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -31,7 +31,7 @@ router.post('/', [
 ], async (req, res) => {
   const errors = validationResult(req);
   if(!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.errors });
+    return res.status(400).json({ errors: errors.array() });
   }
 
   const { email, password } = req.body;
--- routes/api/users.js
+++ routes/api/users.js
@@ -18,7 +18,7 @@ router.post('/', [
 ], async (req, res) => {
   const errors = validationResult(req);
   if(!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.errors });
+    return res.status(400).json({ errors: errors.array() });
   }
 
   const { name, email, password } = req.body;
