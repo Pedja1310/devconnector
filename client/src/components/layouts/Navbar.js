ef0df01c58d8104c62632d8f6e295bee873c51a3
Pedja1310 <pedja1310@gmail.com>
2019-07-15 20:23:19 +0200

    Finish frontend register and logout.

---
+++ client/src/components/layouts/Navbar.js
@@ -0,0 +1,46 @@
+import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { logout } from '../../actions/auth';
+
+const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
+  const userLinks = (
+    <ul>
+      <li>
+        <a href="#" onClick={logout}>
+        <i className="fas fa-sign-out-alt"></i>{' '}
+        <span className="hide-sm">Logout</span></a>
+      </li>  
+    </ul>
+  )
+
+  const guestLinks = (
+    <ul>
+      <li><a to="#">Developers</a></li>
+      <li><Link to="/register">Register</Link></li>
+      <li><Link to="/login">Login</Link></li>
+    </ul>
+  )
+
+  return (
+    <nav className="navbar bg-dark">
+      <h1>
+        <Link to="/"><i className="fas fa-code"></i> DevConnector</Link>
+      </h1>
+      <ul>
+        {!loading && (<Fragment>{isAuthenticated ? userLinks : guestLinks}</Fragment>)}      
+      </ul>
+    </nav>
+  )
+}
+
+Navbar.propType = ({
+  logout: PropTypes.func.isRequired,
+})
+
+const mapStateToProps = state => ({
+  auth: state.auth
+})
+
+export default connect(mapStateToProps, { logout })(Navbar);
